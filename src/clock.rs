dbc4cd8e3afd233a7f90a0862c349cf91eed6ad8
invin1 <marafat@bgsu.edu>
2020-11-01 20:36:33 -0700

    code clean up

--- src/clock.rs
+++ src/clock.rs
@@ -3,7 +3,7 @@ use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-#[derive(Debug)]
+#[derive(Debug, PartialOrd, PartialEq)]
 pub enum Edge {
     Rise,
     Fall,
@@ -14,7 +14,7 @@ pub struct Clock {
     level: Arc<Mutex<u8>>,
     edge: Arc<Mutex<Edge>>,
     stop: Arc<Mutex<bool>>,
-    edge_ct: u64, //edge change time; in millisecond; should be << period
+    edge_tt: u64, // edge transition time; in millisecond; should be << period
     period: u64,  // in millisecond
 }
 
@@ -24,7 +24,7 @@ impl Clock {
             level: Arc::new(Mutex::new(0)),
             edge: Arc::new(Mutex::new(Edge::None)),
             stop: Arc::new(Mutex::new(false)),
-            edge_ct,
+            edge_tt: edge_ct,
             period,
         }
     }
@@ -40,43 +40,45 @@ impl Clock {
             *level = 0;
         }
 
-        let level = Arc::clone(&self.level);
-        let edge = Arc::clone(&self.edge);
-        let stop = Arc::clone(&self.stop);
-        let edge_ct = self.edge_ct;
-        let period = self.period;
+        let c_level = Arc::clone(&self.level);
+        let c_edge = Arc::clone(&self.edge);
+        let c_stop = Arc::clone(&self.stop);
+        let c_edge_tt = self.edge_tt;
+        let c_period = self.period;
 
         thread::spawn(move || {
             loop {
-                {
-                    // break if stop is true
-                    let status = stop.lock().unwrap();
-                    if *status {
-                        break;
-                    }
+                // break if stop is true
+                let status = c_stop.lock().unwrap();
+                if *status {
+                    break;
                 }
-                {
-                    // change edge to up or down
-                    let mut edge = edge.lock().unwrap();
-                    let mut level = level.lock().unwrap();
-                    if *level == 0 {
-                        *edge = Edge::Rise;
-                    } else {
-                        *edge = Edge::Fall;
-                    }
+                drop(status);
+
+                // change edge to rise or fall
+                let mut edge = c_edge.lock().unwrap();
+                let mut level = c_level.lock().unwrap();
+                if *level == 0 {
+                    *edge = Edge::Rise;
+                } else {
+                    *edge = Edge::Fall;
                 }
-                thread::sleep(Duration::from_millis(edge_ct));
-                {
-                    // after edge transition, change edge to none
-                    let mut edge = edge.lock().unwrap();
-                    *edge = Edge::None;
-                }
-                {
-                    // change level to 0 or 1
-                    let mut level = level.lock().unwrap();
-                    *level = (*level + 1) & 1;
-                }
-                thread::sleep(Duration::from_millis(period))
+                drop(level);
+                drop(edge);
+
+                thread::sleep(Duration::from_millis(c_edge_tt));
+
+                // after edge transition, change edge to none
+                let mut edge = c_edge.lock().unwrap();
+                *edge = Edge::None;
+                drop(edge);
+
+                // change level to 0 or 1
+                let mut level = c_level.lock().unwrap();
+                *level = (*level + 1) & 1;
+                drop(level);
+
+                thread::sleep(Duration::from_millis(c_period))
             }
         });
     }
@@ -84,5 +86,28 @@ impl Clock {
     pub fn stop(&mut self) {
         let mut status = self.stop.lock().unwrap();
         *status = true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rs_nor_nochange_0_0() {
+        let mut clock = Clock::new(10, 100);
+        let c_level = Arc::clone(&clock.level);
+
+        clock.start();
+        for i in 1..10 {
+            thread::sleep(Duration::from_millis(clock.period + clock.edge_tt - 1));
+            let level = c_level.lock().unwrap();
+            if *level == 1 {
+                print!("---------");
+            } else {
+                print!("_________");
+            }
+            drop(level);
+        }
     }
 }
